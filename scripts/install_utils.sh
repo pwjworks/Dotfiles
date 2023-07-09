85d7795a02b0d3cbb3c7bc13243a09cb7ecb63b9
pwjworks <13610513466@163.com>
2023-07-10 03:53:50 +0800

    update neovim

--- scripts/install_utils.sh
+++ scripts/install_utils.sh
@@ -1,7 +1,6 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 #logo-ls
 wget https://github.com/Yash-Handa/logo-ls/releases/download/v1.3.7/logo-ls_amd64.deb
 sudo dpkg -i logo-ls_amd64.deb
 
-brew install autojump cmake xmake fzf nodejs lazygit docker fzf lf
+brew install autojump cmake xmake fzf nodejs lazygit docker fzf lf neovim
+
