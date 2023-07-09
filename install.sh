043c81e95742ba3edd655c4fa88c7d181f4bd4ed
pwjworks <13610513466@163.com>
2023-07-10 04:24:13 +0800

    update nvchad

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cp -r nvim ~/.config/nvim
-cp -r custom ~/.config/nvim/lua/custom
 cp zshrc ~/.zshrc
 cp tmux.conf ~/.tmux.conf
 cp my-posh-theme.json ~/my-posh-theme.json
 
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+git clone https://github.com/pwjworks/MyNvchad.git ~/.config/nvim/lua/custom/
