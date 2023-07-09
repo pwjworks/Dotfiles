8a9fe7612c08004ba70f9491979eb3c69d371c5f
pwjworks <13610513466@163.com>
2023-07-10 00:55:47 +0800

    add nvim

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+NVIM_HOME = ~/.config/nvim
+cp nvim $NVIM_HOME
+cp custom $NVIM_HOME/lua/custom
 cp zshrc ~/.zshrc
 cp tmux.conf ~/.tmux.conf
 cp my-posh-theme.json ~/my-posh-theme.json
