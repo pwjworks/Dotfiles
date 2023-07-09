da6129c12afffba439dd6125d008a3bf4b655e20
pwjworks <13610513466@163.com>
2023-07-09 23:31:13 +0800

    zsh plugins

--- scripts/install_shell.sh
+++ scripts/install_shell.sh
@@ -2,7 +2,10 @@
 sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 
-# oh my posh
+# oh-my-posh
 curl -s https://ohmyposh.dev/install.sh | bash -s
