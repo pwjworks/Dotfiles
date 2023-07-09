ddce6e2ac9c28e614918e648f850530f6762830a
pwjworks <13610513466@163.com>
2023-07-10 01:41:02 +0800

    rm shells

--- install.sh
+++ install.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-NVIM_HOME = ~/.config/nvim
-
-bash scripts/init.sh
-bash scripts/install_neovim.sh
-bash scripts/install_shell.sh
-bash scripts/install_utils.sh
-cp nvim $NVIM_HOME
-cp custom $NVIM_HOME/lua/custom
+cp nvim ~/.config/nvim
+cp custom ~/.config/nvim/lua/custom
 cp zshrc ~/.zshrc
 cp tmux.conf ~/.tmux.conf
 cp my-posh-theme.json ~/my-posh-theme.json
