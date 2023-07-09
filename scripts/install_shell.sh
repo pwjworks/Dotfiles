95121d065401c1d4fff34cb10f7da5aba1aa9add
pwjworks <13610513466@163.com>
2023-07-09 18:23:41 +0800

    add oh my posh

--- scripts/install_shell.sh
+++ scripts/install_shell.sh
@@ -1,2 +1,8 @@
+# zsh
 sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# oh my posh
+curl -s https://ohmyposh.dev/install.sh | bash -s
