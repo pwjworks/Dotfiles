d1d642cc91f1eccb8a900aabc8f9b67c02c1a333
pwjworks <13610513466@163.com>
2023-07-10 01:08:14 +0800

    add shells

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 NVIM_HOME = ~/.config/nvim
+
+bash scripts/init.sh
+bash scripts/install_neovim.sh
+bash scripts/install_shell.sh
+bash scripts/install_utils.sh
 cp nvim $NVIM_HOME
 cp custom $NVIM_HOME/lua/custom
 cp zshrc ~/.zshrc
